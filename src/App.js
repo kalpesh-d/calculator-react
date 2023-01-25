c591d2a47950a820b65ec89d1d0d6a49fd25fc8a
Kalpesh Dhoke <kalpeshdhoke740@outlook.com>
2023-01-25 21:30:14 +0530

    defined number click & recreated layout

--- src/App.js
+++ src/App.js
@@ -2,7 +2,14 @@ import { useState } from "react";
 import './App.css';
 
 function App() {
-  const [displayValue, setDisplayValue] = useState('');
+  const [displayValue, setDisplayValue] = useState('0');
+  
+  const handleNumberClick = (e) => {
+    const value = e.target.value;
+    setDisplayValue(displayValue === '0' ? value : displayValue + value);
+  }
+
+
   return (
     <>
       <div className="container">
@@ -10,28 +17,24 @@ function App() {
           <input type="text" value={displayValue}/>
         </form>
         <div className="buttons">
-          <button>Clear</button>
-          <button>C</button>
-          <button>/</button>
-    
-          <button>7</button>
-          <button>8</button>
-          <button>9</button>
-          <button>*</button>
-    
-          <button>4</button>
-          <button>5</button>
-          <button>6</button>
-          <button>-</button>
-    
-          <button>1</button>
-          <button>2</button>
-          <button>3</button>
-    
-          <button>+</button>
-          <button>0</button>
-          <button>.</button>
-          <button>=</button>
+          <button className="button" value="/">/</button>   
+          <button className="button" value="1" onClick={handleNumberClick}>1</button>
+          <button className="button" value="2" onClick={handleNumberClick}>2</button>
+          <button className="button" value="3" onClick={handleNumberClick}>3</button>
+          <button className="button" value="+">+</button>
+          <button className="button" value="4" onClick={handleNumberClick}>4</button>
+          <button className="button" value="5" onClick={handleNumberClick}>5</button>
+          <button className="button" value="6" onClick={handleNumberClick}>6</button>
+          <button className="button" value="-">-</button>
+          <button className="button" value="7" onClick={handleNumberClick}>7</button>
+          <button className="button" value="8" onClick={handleNumberClick}>8</button>
+          <button className="button" value="9" onClick={handleNumberClick}>9</button>
+          <button className="button" value="">*</button>
+          <button className="button" value="c" >C</button>
+          <button className="button" value="0" onClick={handleNumberClick}>0</button>
+          <button className="button" value=".">.</button>
+          <button className="button" value=".">/</button>
+          <button value="=" className="equal">=</button>
         </div>
       </div>
     </>    
