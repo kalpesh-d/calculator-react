39278d8c66a17f8246bb4bf8a541e975c2f63b4f
Kalpesh Dhoke <kalpeshdhoke740@outlook.com>
2023-01-26 19:19:06 +0530

    done hope so.

--- src/App.js
+++ src/App.js
@@ -1,63 +1,58 @@
+import math from "mathjs/lib/browser/math.js";
 import { useState } from "react";
 import './App.css';
 
 function App() {
   const [displayValue, setDisplayValue] = useState('0');
-  const [operations, setOperation] = useState([]);
-  
-  const handleNumberClick = (e) => {
-    const value = e.target.value;
-    setDisplayValue(displayValue === '0' ? value : displayValue + value);
-  }
-
-  const handleOperatorClick = (e) => {
-    const value = e.target.value;
-    setOperation([...operations, displayValue, value]);
-    setDisplayValue('0');
-  }
-
-  const handleEqualClick = () => {
-    setOperation([...operations, displayValue]);
-    const result = eval(operations.join(''));
-    setDisplayValue(result);
-    setOperation([]);
-  }
 
   const handleClearClick = () => {
-    setDisplayValue('0');
-    setOperation([]);
+    setDisplayValue('');
   }
- 
+
+  const handleClick = (e) => {
+    setDisplayValue(displayValue.concat(e.target.value));
+  }
+
+  const handleBackSpace = () => {
+    setDisplayValue(displayValue.slice(0, -1));
+  }
+
+  const handleCalculate = () => {
+    try {
+      setDisplayValue(eval(displayValue).toString());
+    } catch (err) {
+      setDisplayValue(err);
+    }
+  }
+
   return (
     <>
       <div className="container">
-        <form className="input">
-          <input type="text" value={displayValue}/>
-        </form>
+          <input type="text" value={displayValue} maxLength='1'/>
         <div className="buttons">
-          <button className="button" value="c" onClick={handleClearClick}>C</button>
-          <button className="button" value="+/-">+/-</button>   
-          <button className="button" value="/">/</button>   
+          <button className="button" value="clear" onClick={handleClearClick}>Clear</button>
+          <button className="button" value="c" onClick={handleBackSpace}>c</button>   
+          <button className="button" value="/" onClick={handleClick}>/</button>   
 
-          <button className="button" value="7" onClick={handleNumberClick}>7</button>
-          <button className="button" value="8" onClick={handleNumberClick}>8</button>
-          <button className="button" value="9" onClick={handleNumberClick}>9</button>
-          <button className="button" value="*" onClick={handleOperatorClick}>x</button>
+          <button className="button" value="7" onClick={handleClick}>7</button>
+          <button className="button" value="8" onClick={handleClick}>8</button>
+          <button className="button" value="9" onClick={handleClick}>9</button>
+          <button className="button" value="*" onClick={handleClick}>x</button>
 
-          <button className="button" value="4" onClick={handleNumberClick}>4</button>
-          <button className="button" value="5" onClick={handleNumberClick}>5</button>
-          <button className="button" value="6" onClick={handleNumberClick}>6</button>
-          <button className="button" value="-" onClick={handleOperatorClick}>-</button>
+          <button className="button" value="4" onClick={handleClick}>4</button>
+          <button className="button" value="5" onClick={handleClick}>5</button>
+          <button className="button" value="6" onClick={handleClick}>6</button>
+          <button className="button" value="-" onClick={handleClick}>-</button>
 
-          <button className="button" value="1" onClick={handleNumberClick}>1</button>
-          <button className="button" value="2" onClick={handleNumberClick}>2</button>
-          <button className="button" value="3" onClick={handleNumberClick}>3</button>
-          <button className="button" value="+" onClick={handleOperatorClick}>+</button>
+          <button className="button" value="1" onClick={handleClick}>1</button>
+          <button className="button" value="2" onClick={handleClick}>2</button>
+          <button className="button" value="3" onClick={handleClick}>3</button>
+          <button className="button" value="+" onClick={handleClick}>+</button>
 
           
-          <button className="button" value=".">.</button>
-          <button className="button" value="0" onClick={handleNumberClick}>0</button>
-          <button className="button" value="=" onClick={handleEqualClick}>=</button>
+          <button className="button" value="." onClick={handleClick}>.</button>
+          <button className="button" value="0" onClick={handleClick}>0</button>
+          <button className="button" value="=" onClick={handleCalculate}>=</button>
         </div>
       </div>
     </>    
