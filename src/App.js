d08305c1481f56258cff524868886d54635cb324
Kalpesh Dhoke <kalpeshdhoke740@outlook.com>
2023-01-26 14:03:07 +0530

    change in layout

--- src/App.js
+++ src/App.js
@@ -3,38 +3,61 @@ import './App.css';
 
 function App() {
   const [displayValue, setDisplayValue] = useState('0');
+  const [operations, setOperation] = useState([]);
   
   const handleNumberClick = (e) => {
     const value = e.target.value;
     setDisplayValue(displayValue === '0' ? value : displayValue + value);
   }
 
+  const handleOperatorClick = (e) => {
+    const value = e.target.value;
+    setOperation([...operations, displayValue, value]);
+    setDisplayValue('0');
+  }
 
+  const handleEqualClick = () => {
+    setOperation([...operations, displayValue]);
+    const result = eval(operations.join(''));
+    setDisplayValue(result);
+    setOperation([]);
+  }
+
+  const handleClearClick = () => {
+    setDisplayValue('0');
+    setOperation([]);
+  }
+ 
   return (
     <>
       <div className="container">
-        <form>
+        <form className="input">
           <input type="text" value={displayValue}/>
         </form>
         <div className="buttons">
+          <button className="button" value="c" onClick={handleClearClick}>C</button>
+          <button className="button" value="+/-">+/-</button>   
           <button className="button" value="/">/</button>   
-          <button className="button" value="1" onClick={handleNumberClick}>1</button>
-          <button className="button" value="2" onClick={handleNumberClick}>2</button>
-          <button className="button" value="3" onClick={handleNumberClick}>3</button>
-          <button className="button" value="+">+</button>
-          <button className="button" value="4" onClick={handleNumberClick}>4</button>
-          <button className="button" value="5" onClick={handleNumberClick}>5</button>
-          <button className="button" value="6" onClick={handleNumberClick}>6</button>
-          <button className="button" value="-">-</button>
+
           <button className="button" value="7" onClick={handleNumberClick}>7</button>
           <button className="button" value="8" onClick={handleNumberClick}>8</button>
           <button className="button" value="9" onClick={handleNumberClick}>9</button>
-          <button className="button" value="">*</button>
-          <button className="button" value="c" >C</button>
-          <button className="button" value="0" onClick={handleNumberClick}>0</button>
+          <button className="button" value="*" onClick={handleOperatorClick}>x</button>
+
+          <button className="button" value="4" onClick={handleNumberClick}>4</button>
+          <button className="button" value="5" onClick={handleNumberClick}>5</button>
+          <button className="button" value="6" onClick={handleNumberClick}>6</button>
+          <button className="button" value="-" onClick={handleOperatorClick}>-</button>
+
+          <button className="button" value="1" onClick={handleNumberClick}>1</button>
+          <button className="button" value="2" onClick={handleNumberClick}>2</button>
+          <button className="button" value="3" onClick={handleNumberClick}>3</button>
+          <button className="button" value="+" onClick={handleOperatorClick}>+</button>
+
+          
           <button className="button" value=".">.</button>
-          <button className="button" value=".">/</button>
-          <button value="=" className="equal">=</button>
+          <button className="button" value="0" onClick={handleNumberClick}>0</button>
+          <button className="button" value="=" onClick={handleEqualClick}>=</button>
         </div>
       </div>
     </>    
